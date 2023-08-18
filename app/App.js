2140e37f8adb1ad4d3b8e3227976393af76894dd
Yosuke Santamaria <youman.318@gmail.com>
2023-08-18 13:46:39 +0900

    calendar

--- app/App.js
+++ app/App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import {Calendar} from "react-native-calendars";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+
+
+                <Calendar
+                    style={{
+                        height: '100%'
+                    }}
+                    theme={{
+                        'stylesheet.calendar.main': {
+                            monthView: {
+                                flex: 1,
+                                height: '100%',
+                                justifyContent: 'space-around'
+                            },
+                            week: {
+                                flex: 1,
+                                marginVertical: 0,
+                                flexDirection: 'row',
+                                justifyContent: 'space-around'
+                            },
+                            dayContainer: {
+                                borderColor: '#f5f5f5',
+                                borderWidth: 1,
+                                flex: 1,
+                            },
+                        }
+                    }}
+                />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
