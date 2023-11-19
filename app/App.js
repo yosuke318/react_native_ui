6e67ce51c4b15001c80bdffe9958c2e7248c4d07
Yosuke Santamaria <youman.318@gmail.com>
2023-11-19 14:04:32 +0900

    Componentized Calendar is button.

--- app/App.js
+++ app/App.js
@@ -1,12 +1,28 @@
+import CalendarComponent from "./components/CalendarComponent";
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import {View, Button} from 'react-native';
 
-import CalendarComponent from "./CalendarComponent";
+
+const Stack = createStackNavigator();
 
 export default function App() {
     return (
-
-
-        <CalendarComponent />
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen}/>
+                <Stack.Screen name="Calendar" component={CalendarComponent}/>
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
 
-
+export const HomeScreen = ({navigation}) => {
+    return (
+        <View>
+            {/* ボタンを押して画面遷移 */}
+            <Button title="カレンダー" onPress={() => navigation.navigate('Calendar')} />
+        </View>
+    );
+}
